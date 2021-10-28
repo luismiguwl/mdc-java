98ee7ab36564646a3d7de0ce9880c6d6e153fdf3
luismiguwl <luisbwrb@gmail.com>
2021-10-28 16:54:09 -0300

    refatorando métodos e aprimorando a exibição

--- src/com/luis/mdc/operacional/MDC.java
+++ src/com/luis/mdc/operacional/MDC.java
@@ -1,30 +1,35 @@
 package com.luis.mdc.operacional;
 
 import java.util.Arrays;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class MDC {
 
     private int[] numeros;
     private int mdc;
     private int primoAtual;
+    private Vector<Integer> divisoresDeTodos;
 
     public MDC(int[] numeros) {
         this.numeros = numeros;
         mdc = 1;
         primoAtual = 2;
+        divisoresDeTodos = new Vector<>(numeros.length);
     }
 
     public int calcular() {
         if (todosOsNumerosSaoIguais()) {
             return numeros[0];
         }
-        
+
         while (numerosSaoDiferentesDeUm()) {
             if (primoAtualEhDivisorDeAlgumNumero()) {
                 exibirMacete();
-                
+
                 if (primoAtualEhDivisorDeTodosOsNumeros()) {
                     mdc *= primoAtual;
+                    divisoresDeTodos.add(primoAtual);
                 }
 
                 dividirNumerosPeloPrimoAtual();
@@ -35,7 +40,7 @@ public class MDC {
 
         return mdc;
     }
-    
+
     private boolean todosOsNumerosSaoIguais() {
         int primeiroNumero = numeros[0];
         return Arrays.stream(numeros).allMatch(numero -> numero == primeiroNumero);
@@ -89,21 +94,38 @@ public class MDC {
 
         return true;
     }
-    
+
     private void exibirMacete() {
         String[] numerosEmString = converterNumerosParaArrayDeString();
         String macete = String.join(", ", numerosEmString) + " | " + primoAtual;
-        System.out.println(macete);
+        System.out.print(macete);
+
+        if (primoAtualEhDivisorDeTodosOsNumeros()) {
+            System.out.println(" -> divisor de todos");
+        } else {
+            System.out.println("");
+        }
     }
-    
+
     private String[] converterNumerosParaArrayDeString() {
         String[] array = new String[numeros.length];
-        
+
         for (int i = 0; i < numeros.length; i++) {
             array[i] = Integer.toString(numeros[i]);
         }
-        
+
         return array;
     }
 
+    public void exibirResultadoDaMultiplicacaoDosNumerosSeHouverMaisDeUmNumeroQueDividePorTodos() {
+        if (divisoresDeTodos.size() > 1) {
+            String multiplicacao = divisoresDeTodos.stream()
+                    .map(numero -> Integer.toString(numero))
+                    .collect(Collectors.joining(" * "));
+
+            System.out.println("\n" + multiplicacao + " = " + mdc + "\n");
+        } else {
+            System.out.println("\nMDC: " + mdc);
+        }
+    }
 }
--- src/com/luis/principal/Main.java
+++ src/com/luis/principal/Main.java
@@ -5,11 +5,10 @@ import com.luis.mdc.operacional.MDC;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numeros = {12, 20, 24};
+        int[] numeros = {12, 21, 24};
         MDC mdc = new MDC(numeros);
 
         int resultado = mdc.calcular();
-
-        System.out.println("MDC: " + resultado);
+        mdc.exibirResultadoDaMultiplicacaoDosNumerosSeHouverMaisDeUmNumeroQueDividePorTodos();
     }
 }
